d919b59bd5e68b5d75bbed7b876e5ec54532d345
Nacho <theignaciocordoba@gmail.com>
2024-01-22 11:36:53 -0500

    feat: add stimulus

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+declare global {
+  interface Window {
+    Stimulus: Application;
+  }
+}
+
+window.Stimulus = Application.start();
+const context = require.context("./controllers", true, /\.ts$/);
+window.Stimulus.load(definitionsFromContext(context));
+
 window.onload = () => {
   // Your code here
   console.log("Hello World!");
